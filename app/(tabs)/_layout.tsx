5c3d410f406cdb94fd3f0cc333e22a02d57383bb
Marco Aurelio <macordons@unal.edu.co>
2026-03-01 16:07:50 -0500

    Primer commit: base limpia de Carrangapp

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,49 @@
+import { Tabs } from "expo-router";
+import React from "react";
+
+import { HapticTab } from "@/components/haptic-tab";
+import { IconSymbol } from "@/components/ui/icon-symbol";
+import { Colors } from "@/constants/theme";
+import { useColorScheme } from "@/hooks/use-color-scheme";
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        headerShown: false,
+        tabBarButton: HapticTab,
+      }}
+    >
+      <Tabs.Screen
+        name="Inicio"
+        options={{
+          title: "Inicio",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="house.fill" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Repertorio"
+        options={{
+          title: "Repertorio",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="book.fill" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Comunidad"
+        options={{
+          title: "Comunidad",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="person.2.fill" color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
