e646aa5fcb3d1b9f9ea7f411f684e5844a6e361b
Marco Aurelio <macordons@unal.edu.co>
2026-03-01 18:01:24 -0500

    Update firebase.ts

--- services/firebase.ts
+++ services/firebase.ts
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAkvHIXV1NR2t6_FQV-7baEq2TEinD8-3I",
+  apiKey: "",
   authDomain: "carrangapp.firebaseapp.com",
   projectId: "carrangapp",
   storageBucket: "carrangapp.firebasestorage.app",
   messagingSenderId: "562780981106",
-  appId: "1:562780981106:web:670261e5a8629d9af23feb",
+  appId: "",
   measurementId: "G-8LSZWMCGTP",
 };
 
